94c06ea72514d89050cca50cd35fce750c94e279
Marco Jimenez <marcojm0690@hotmail.com>
2020-07-16 18:38:54 -0600

    Agregar views

---
+++ Seguros/Controllers/InsuranceController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Seguros.Controllers
+{
+    public class InsuranceController : Controller
+    {
+        public ActionResult Add()
+        {
+            ViewBag.Title = "Agregar Seguro";
+
+            return View();
+        }
+
+        public ActionResult Query()
+        {
+            ViewBag.Title = "Consultar Seguro";
+
+            return View();
+        }
+        public ActionResult Delete()
+        {
+            ViewBag.Title = "Eliminar Seguro";
+
+            return View();
+        }
+        public ActionResult Modify()
+        {
+            ViewBag.Title = "Modificar Seguro";
+
+            return View();
+        }
+    }
+}
